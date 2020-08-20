9d7ff5090bbbf19b32191fa252fdfd8f3f31a4bb
Alex Dewar <alex.dewar90@gmail.com>
2020-08-20 19:04:13 +0100

    scsi: mptfusion: Remove unnecessarily casts
    In a number of places, the value returned from pci_alloc_consistent() is
    unnecessarily cast from void*. Remove these casts.
    
    Issue identified with Coccinelle.
    
    Signed-off-by: Alex Dewar <alex.dewar90@gmail.com>

--- drivers/message/fusion/mptfc.c
+++ drivers/message/fusion/mptfc.c
@@ -763,7 +763,7 @@ mptfc_GetFcPortPage0(MPT_ADAPTER *ioc, int portnum)
 
 	data_sz = hdr.PageLength * 4;
 	rc = -ENOMEM;
-	ppage0_alloc = (FCPortPage0_t *) pci_alloc_consistent(ioc->pcidev, data_sz, &page0_dma);
+	ppage0_alloc = pci_alloc_consistent(ioc->pcidev, data_sz, &page0_dma);
 	if (ppage0_alloc) {
 
  try_again:
@@ -904,9 +904,9 @@ start_over:
 		if (data_sz < sizeof(FCPortPage1_t))
 			data_sz = sizeof(FCPortPage1_t);
 
-		page1_alloc = (FCPortPage1_t *) pci_alloc_consistent(ioc->pcidev,
-						data_sz,
-						&page1_dma);
+		page1_alloc = pci_alloc_consistent(ioc->pcidev,
+						   data_sz,
+						   &page1_dma);
 		if (!page1_alloc)
 			return -ENOMEM;
 	}
